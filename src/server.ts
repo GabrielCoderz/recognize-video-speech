e1435a6d7c1d8f2ce90d897873a88eac566a88d5
GabrielCoderz <biel.s.maia@hotmail.com>
2024-02-27 23:59:04 -0300

    chore: clean the code

--- src/server.ts
+++ src/server.ts
@@ -15,15 +15,6 @@ ffmpeg.setFfmpegPath(path.path);
 
 app.get("/transcribe", new transcribeAudioController().handle);
 
-// app.get("/", async (req, res) => {
-//   try {
-//     await getAudio("https://www.youtube.com/shorts/FVWcZXuZmv8");
-//     res.send("ok");
-//   } catch (err) {
-//     res.send(err);
-//   }
-// });
-
 app.listen(4000, () => {
   console.log(`server running on port 4000`);
 });
